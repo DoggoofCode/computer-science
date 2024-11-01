1b2d05b3459dea3e53189cd5808376a666287b57
vedjaggi <vedajaggi@gmail.com>
2024-11-01 14:47:58 +0800

    Step 8

--- main.py
+++ main.py
@@ -17,7 +17,25 @@ class FullSolve:
         pass
 
 def main():
-    pass
+    # Adds a title to the Sidebar
+    st.sidebar.title("Model Selection")
+
+    # Adds a selection box to the Sidebar
+    model_selection = st.sidebar.selectbox(
+        "Select a page:",
+        [
+            "Title Page",
+            "Single Solve",
+            "Full Solve",
+        ]
+    )
+    if model_selection == "Title Page":
+        TitlePage.Run()
+    elif model_selection == "Single Solve":
+        SingleSolve.Run()
+    elif model_selection == "Full Solve":
+        FullSolve.Run()
+
 
 if __name__ == '__main__':
     main()
