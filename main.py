744f1c89c92591ca23e319ee5592ba9f35a5565d
vedjaggi <vedajaggi@gmail.com>
2024-11-01 14:40:03 +0800

    Step 6

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import streamlit as st
 import numpy as np
 
+class TitlePage:
+    @classmethod
+    def Run(cls):
+        pass
+
+class SingleSolve:
+    @classmethod
+    def Run(cls):
+        pass
+
+class FullSolve:
+    @classmethod
+    def Run(cls):
+        pass
+
 def main():
     pass
 
